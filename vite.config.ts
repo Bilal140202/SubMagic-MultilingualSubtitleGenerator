9e05a821d2cfc784e92b23be66e6af63746e422d
Ansari Mohammad Bilal <bilalansari1402@gmail.com>
2025-07-10 13:09:35 +0530

    Fix Vite development server errors

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['@xenova/transformers']
+    exclude: ['@xenova/transformers'],
+    include: ['onnxruntime-web']
   },
   server: {
     headers: {
